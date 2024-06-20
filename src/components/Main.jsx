ebbc2fb63025af9da995f3d88a907b2c1dabc3cd
Эльчибекова Хайриниса <elcinisa@gmail.com>
2024-06-20 21:24:33 +0300

    create folders

---
+++ src/components/Main.jsx
@@ -0,0 +1,36 @@
+import "./Main.css";
+
+import { MyButton } from "./uiButton/MyButton";
+
+export function Main({ movie, deleteMovieHandler }) {
+
+  return (
+    <main className="main">
+      <h2>АКЫРКЫ ЧЫККАН ТАСМАЛАР</h2>
+      <section className="movies_container">
+        {movie.map((item) => (
+          <div className="movie-block" key={item.movieId}>
+            <img className="movie-images" src={item.movieImage} alt="Чон кыз" />
+            <article className="wrapper_info">
+              <div>
+                <h4 className="movie-title">{item.movieTitle}</h4>
+                <p className="movie-status">{item.movieStatus}</p>
+              </div>
+              <div className="wrapperButtons">
+                <MyButton
+                  className="buyBtn"
+                  onClick={() => deleteMovieHandler(item.movieId)}
+                >
+                  Удалить
+                </MyButton>
+                <MyButton className="buyBtn">Изменить</MyButton>
+              </div>
+            </article>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
+
+export default Main;
